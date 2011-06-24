72f112650fa644f1cb59fcd89d7ae0f8a5a05e8c
Lishaak Bystroushaak <bystrousak@kitakitsune.org>
2011-06-24 18:22:42 +0200

    Looks like this should work.

--- parts/gear.d
+++ parts/gear.d
@@ -1,7 +1,32 @@
 module parts.gear;
 
+import std.string;
 import parts.part;
 
 public class Gear : Part{
+	this(){} // kinky :S
 	
+	// Parse records defined as "Type : name"
+	this(string line){
+		if (line.indexOf(':') <= 0)
+			throw new PartDefinitionException("No definition!");
+		
+		string[] tmp = line.split(":");
+
+		if (tmp[0].strip().tolower() != "gear")
+			throw new PartDefinitionException("Unknown part '" ~ tmp[0].strip() ~ "'");
+
+		this.name = tmp[1].strip().tolower();
+	}
+
+	private Rotation reaction(Rotation nr){
+		Rotation[Rotation] table;
+
+		table[Rotation.Left]  = Rotation.Right;
+		table[Rotation.Right] = Rotation.Left;
+		table[Rotation.None]  = Rotation.None;
+		table[Rotation.Any]   = Rotation.Any;
+
+		return table[nr];
+	}
 }
--- parts/part.d
+++ parts/part.d
@@ -2,13 +2,18 @@ module parts.part;
 
 public enum Rotation {Right, Left, None, Any};
 
-public class Part{
-	private bool checked = false;
-	private string name;
-	private uint gear_num;
-	private Rotation r = Rotation.None;
+public struct PartContainer{
+	Part part;
+	Rotation rotation;
+}
 
-	private Part[] neigbours;
+public abstract class Part{
+	protected bool rotating = false;
+	protected string name;
+	protected uint gear_num;
+	protected Rotation r = Rotation.None;
+
+	protected Part[] neighbours;
 
 	public string getName(){
 		return this.name;
@@ -17,16 +22,44 @@ public class Part{
 		this.name = new_name;
 	}
 
-	public bool isChecked(){
-		return this.checked;
-	}
-	public void setChecked(bool checked){
-		this.checked = checked;
+	public bool isRotating(){
+		return this.rotating;
 	}
 
-	public void addNeighbour(Part neigbour){
-		neigbours ~= neigbour;
+	public void addNeighbour(Part neighbour){
+		this.neighbours ~= neighbour;
 	}
 
-	public abstract Part[] react(Rotation new_r);
+	protected abstract Rotation reaction(Rotation);
+	
+	public PartContainer[] react(Rotation neigh_r){
+		PartContainer[] output;
+
+		if (!this.rotating){
+			this.r = this.reaction(neigh_r);
+
+			PartContainer p;
+			foreach(n; this.neighbours){
+				p.part = n;
+				p.rotation = this.r;
+			}
+		}else{
+			if (this.reaction(neigh_r) != this.r && this.r != Rotation.Any)
+				throw new RotationCollisionException(this.name);
+		}
+
+		return output;
+	}
+}
+
+public class PartDefinitionException : Exception{
+	this(string msg){
+		super(msg);
+	}
+}
+
+public class RotationCollisionException : Exception{
+	this(string msg){
+		super(msg);
+	}
 }
